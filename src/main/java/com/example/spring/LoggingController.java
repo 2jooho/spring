d89a0f0dd9623f4c667c9968689996679687624a
jooho <tjrwn1003@gmail.com>
2022-07-06 00:01:02 +0900

    test2

--- src/main/java/com/example/spring/LoggingController.java
+++ src/main/java/com/example/spring/LoggingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoggingController {
 
-    @Value("${logging-module.version}")
     private String version;
 
     @GetMapping("/")
